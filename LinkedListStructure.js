9ddad58053e12c73adff919f6b5ed2b0b6d95edf
dpirvoiu <adisor.pdw@gmail.com>
2023-05-13 10:25:54 +0300

    chore: Linked List Structure

---
+++ LinkedListStructure.js
@@ -0,0 +1,20 @@
+// Example of linked list
+
+const linkedList = {
+  head: {
+    value: 11,
+      next: {
+       value: 3,
+        next: {
+         value:23,
+          next:{
+           value: 7,
+            next: {
+             value: 4,
+              next: null // TAIL
+            }
+          }
+        }
+    }
+  }
+}
