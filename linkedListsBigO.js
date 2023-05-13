8a5f212b21889a51696f61b6c9c2d86888111a7e
dpirvoiu <adisor.pdw@gmail.com>
2023-05-13 10:13:18 +0300

    chore: Performance

---
+++ linkedListsBigO.js
@@ -0,0 +1,23 @@
+// Big O end ->  O(1) Add last item
+// Big O end->  O(n) Remove last item
+
+// Big O start -> O(1) Add first item
+// Big O start -> O(1) Remove from start
+
+// Insert to middle -> O(n)
+// Find an item by value -> O(n)
+// Find an item by index -> O(n)
+
+/*        Linked List                       Array
+   PUSH      O(1)                            O(1)
+   POP       O(n)                            O(1)  // Arrays are better
+   Shift     O(1)                            O(n)  // Better for Linked Lists
+   Unshift   O(1)                            O(n)  // BEtter for Linked Lists
+   Insert    O(n)                            O(n)
+   Delete    O(n)                            O(n)
+Find by Idx  O(n)                            O(1) // Arrays are better
+Find by Val  O(n)                            O(n)
+ */
+
+git config --global user.email "adisor.pdw@gmail.com"
+git config --global user.name "dpirvoiu"
