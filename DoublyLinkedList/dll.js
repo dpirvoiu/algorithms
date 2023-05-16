bb1a9a64bc1d3c7d55ebd2a9d4e7e536c6790a4a
dpirvoiu <adisor.pdw@gmail.com>
2023-05-16 20:39:53 +0300

    chore: dll pop

--- DoublyLinkedList/dll.js
+++ DoublyLinkedList/dll.js
@@ -28,5 +28,20 @@ class DoublyLinkedList {
     return this;
   }
 
+  pop() {
+    if (this.length === 0) return undefined;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let temp = this.tail;
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      temp.prev = null;
+      this.length--;
+      return temp;
+    }
+  }
+
 
 }
