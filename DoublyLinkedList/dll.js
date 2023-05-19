558f7f6c43c8248c89ed8a2f8b8a5baa4a04a624
dpirvoiu <adisor.pdw@gmail.com>
2023-05-19 06:50:58 +0300

    chore: continue

--- DoublyLinkedList/dll.js
+++ DoublyLinkedList/dll.js
@@ -116,4 +116,18 @@ class DoublyLinkedList {
     this.length++;
     return true;
   }
+
+  remove(index) {
+    if (index === 0) return this.shift(index);
+    if (index === this.length - 1) return this.pop();
+    if (index < 0 || index >= this.length) return undefined;
+
+    let temp = this.get(index);
+    temp.prev.next = temp.next;
+    temp.next.prev = temp.prev;
+    temp.next = null;
+    temp.prev = null;
+    this.length--;
+    return temp;
+  }
 }
